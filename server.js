209c0892fbe3d7744e571d90ac0bb9b4e931bd68
SarahVee <sarahvacio@gmail.com>
2022-11-14 12:58:14 -0500

    install pkg and created model directory

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-//server data
+//server data, modules needed for serverjs to run
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var morgan = require('morgan');
-var User = require('./models/user');
+var User = require('./models/user'); //used to express what infor needed to be in database
 var hbs = require('express-handlebars'); 
 var path = require('path'); 
 
